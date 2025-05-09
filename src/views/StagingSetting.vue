67392ce145a31543dc95ba15d835370b81579dc9
m7medNaim <mmednaim24@gmail.com>
2025-05-09 12:30:44 +0300

    updates

--- src/views/StagingSetting.vue
+++ src/views/StagingSetting.vue
@@ -1,17 +1,13 @@
 <template>
-  <div class="mt-4 me-2 bg-white p-4 pb-0 rounded-3">
+  <div class="mt-4 me-2 bg-white pb-0 pt-4 rounded-3 tableStage">
     <form>
-      <h5 class="mb-4">Staging Settings</h5>
-      <div class="row my-3 border-bottom pb-2 mb-4">
+      <h5 class="mb-4 ps-4">Staging Settings</h5>
+      <div class="row my-3 border-bottom pb-2 mb-4 headerStage p-4">
         <div class="col-4">Stage Name</div>
         <div class="col-4">Status</div>
         <div class="col-4">Days Counts</div>
       </div>
-      <div
-        v-for="(stage, index) in stages"
-        :key="index"
-        class="row mb-3 tableStage"
-      >
+      <div v-for="(stage, index) in stages" :key="index" class="row mb-3 p-4">
         <div class="col-12">
           <div
             class="row align-items-center mb-3 border-bottom pb-2"
@@ -80,6 +76,18 @@ export default {
 </script>
 
 <style scoped>
+.tableStage {
+  height: 88vh;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+.headerStage {
+  position: sticky;
+  top: -4%;
+  background-color: #fff;
+  width: 100% !important;
+  z-index: 99;
+}
 .form-check-input:checked {
   background-color: #09b103 !important;
   border-color: #09b103 !important;
